3657a24461d3973933dd4642034fc7ee7ca37026
Carlos Tornadijo <tornadijo@gmail.com>
2014-04-07 23:35:19 +0200

    subo todo

---
+++ app/src/main/java/com/icisneros/conversoreuro/ConversorEuroAppActivity.java
@@ -0,0 +1,15 @@
+package com.icisneros.conversoreuro;
+
+import com.icisneros.conversoreuro.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConversorEuroAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
